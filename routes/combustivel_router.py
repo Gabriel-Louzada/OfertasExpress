b059cc1f3e9432792b780c95477240af3f7e9283
TariagamiDEV <Matheusmilheiros04@gmail.com>
2024-08-23 22:46:46 -0300

    criado todas tabelas de combustivel e também ajustado rotas

--- routes/combustivel_router.py
+++ routes/combustivel_router.py
@@ -8,9 +8,23 @@ templates = Jinja2Templates(directory = "templates")
 
 @router.get("/gasolina_comum", response_class=HTMLResponse)
 async def get_raiz(request: Request):
-    return templates.TemplateResponse("/combustivel/gasolina_comum.html", {"request": request})
+    return templates.TemplateResponse("/combustivel/combustivel_gas_comum.html", {"request": request})
 
 
 @router.get("/gasolina_adtivada", response_class=HTMLResponse)
 async def get_raiz(request: Request):
-    return templates.TemplateResponse("/combustivel/gasolina_adtivada.html", {"request": request})
+    return templates.TemplateResponse("/combustivel/combustivel_gas_adtivada.html", {"request": request})
+
+@router.get("/combustivel_etanol", response_class=HTMLResponse)
+async def get_raiz(request: Request):
+    return templates.TemplateResponse("/combustivel/combustivel_etanol.html", {"request": request})
+
+@router.get("/combustivel_diesels10", response_class=HTMLResponse)
+async def get_raiz(request: Request):
+    return templates.TemplateResponse("/combustivel/combustivel_diesels10.html", {"request": request})
+
+@router.get("/combustivel_diesels500", response_class=HTMLResponse)
+async def get_raiz(request: Request):
+    return templates.TemplateResponse("/combustivel/combustivel_diesels500.html", {"request": request})
+
+
