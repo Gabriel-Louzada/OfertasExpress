4c48b135e49dc87fe00d38d1713452354b635722
TariagamiDEV <Matheusmilheiros04@gmail.com>
2024-08-24 01:55:41 -0300

    alteração de rota botaão homepage. Alterado navbar(cor, botão de pesquisa e feito limpeza do cód.)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from repo.combustivel_posto_repo import criar_tabela_combustivel_posto
 from repo.combustivel_repo import criar_tabela_combustivel
 from repo.posto_repo import criar_tabela_posto
@@ -11,12 +12,14 @@ criar_tabela_combustivel()
 criar_tabela_combustivel_posto()
 
 app = FastAPI()
+
+app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
+
 app.include_router(main_router.router)
 app.include_router(combustivel_router.router)
 
 print("Tudo certo no MAIN")
 
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001 )
+    uvicorn.run(app, host="127.0.0.1",port=8001)
